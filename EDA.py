210b0e3d0bf0371714ec094ad8f8a020328ce4b1
rishaabhgautham-del <rishaabh.gautham@gmail.com>
2026-02-15 21:18:10 -0600

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,8 +1,16 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import datetime as dt
 
 #Importing a dataset
 df = pd.read_csv("Unicorn_Companies.csv")
 
+#Inspecting the first 10 rows
+first_10 = df.head(10)
+
+#The size and shape of the dataset
+size = df.size #Calculates the number of elements in the dataset
+shape = df.shape #Returns a tuple in the (rows, columns) format
+
+#Other information about the dataset
+info = df.info()
